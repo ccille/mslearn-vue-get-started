99983a171d133f21ff831dddcad190c344809316
Cecilie Maria Hernig <ceci7542@edu.easj.dk>
2022-10-27 14:48:24 +0200

    Done

--- start/index.js
+++ start/index.js
@@ -3,8 +3,15 @@ const app = Vue.createApp({
         return {
             productName: 'Book a Cruise to the moon',
             productDescription: 'Cruise to the moon in our luxious shuttle. Watch the astronauts working outside the International space station.',
+            
             //Add additional properties later
+            classObjects: { centered: true, active: true},
+            styleObject: {'background-color': 'red'},
+            productImage: './assets/cruise.jpg',
+            productImageDescription: 'An astronaut floats outside the window while you sit in comfort',
+            productImageStyle: { 'border-radius': '15px'}
+            
         }
     }
-})
+}).mount('#app')
 
