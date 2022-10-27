59fc987c47d2da5fcc04e1030c39478bc785a3f1
Cecilie Maria Hernig <ceci7542@edu.easj.dk>
2022-10-27 13:59:02 +0200

    Import Vue og oprettet index.js fil + indhold i

---
+++ start/index.js
@@ -0,0 +1,10 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            productName: 'Book a Cruise to the moon',
+            productDescription: 'Cruise to the moon in our luxious shuttle. Watch the astronauts working outside the International space station.',
+            //Add additional properties later
+        }
+    }
+})
+
